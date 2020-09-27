7950d12dbe15dd151ae4aac91c966ed38510b247
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 03:03:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
-def fizzbuzz(int)
- if int % 3 == 0
-    "Fizz"
- if int % 5 == 0
-    "Buzz"
+1.upto(100) do |i|
+    if i % 5 == 0 and i % 3 == 0
+      puts "FizzBuzz"
+     elsif i % 5 == 0
+      puts "Buzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    else
+       puts i
   end
 end
