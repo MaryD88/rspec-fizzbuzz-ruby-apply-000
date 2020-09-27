a0bb6b65d82f5799c18f1c083b941731e0fff7ee
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 02:22:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-upto(100) do |i|
-      if i % 5 == 0 and i % 3 == 0
-        puts "FizzBuzz"
-      elsif i % 5 == 0
-        puts "Buzz"
-      elsif i % 3 == 0
-        puts "Fizz"
-      else
-        puts i
-      end
-    end
+puts('Lets play fizzbuzz')
+print('enter a number: ')
+number = gets()
+puts(number)
+if number == % 3
+  puts ('fizz')
+elsif number == % 5
+  puts ('buzz')
+elsif number == %15
+  puts ('fizzbuzz')
+end
