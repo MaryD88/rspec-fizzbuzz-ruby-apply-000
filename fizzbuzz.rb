0dcdb9c2475756dd290f62da5595fb18a81a0c88
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 02:29:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,52 @@
-def fizzbuzz(n)
-  (1..n).each do |i|
-    if i % 3 == 0 && i % 5 == 0
-      puts 'fizzbuzz'
-    elsif i % 3 == 0
-      puts 'fizz'
-    elsif i % 5 == 0
-      puts 'buzz'
+
+def fizz_buzz_1(max)
+  arr = []
+  (1..max).each do |n|
+    if ((n % 3 == 0) && (n % 5 == 0))
+      arr << "FizzBuzz"
+    elsif (n % 3 == 0)
+      arr << "Fizz"
+    elsif (n % 5 == 0)
+      arr << "Buzz"
     else
-      puts i
+      arr << n
     end
   end
+  return arr
 end
 
-fizzbuzz(100)
+def fizz_buzz_2(max)
+  arr = []
+  (1..max).each do |n|
+    if (n % 3 == 0)
+      if (n % 5 == 0)
+        arr << "FizzBuzz"
+      else
+        arr << "Fizz"
+      end
+    elsif (n % 5 == 0)
+      arr << "Buzz"
+    else
+      arr << n
+    end
+  end
+  return arr
+end
+
+def fizz_buzz_3(max)
+  arr = []
+  (1..max).each do |n|
+    text = ""
+    if (n % 3 == 0)
+      text << "Fizz"
+    end
+    if (n % 5 == 0)
+      text << "Buzz"
+    end
+    if !((n % 3 == 0) || (n % 5 == 0))
+      text = n
+    end
+    arr << text
+  end
+  return arr
+end
