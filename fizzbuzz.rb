82c2d656474eafb7c13f213c05908e5ddffa5270
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 03:07:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(int)
   if int % 5 == 0 and int % 3 == 0
     puts "FizzBuzz"
-  elsif int % 5 == 0
+  if int % 5 == 0
     puts "Buzz"
-  elsif int % 3 == 0
+  if int % 3 == 0
     puts "Fizz"
+  if int % 4 == 0
   end
 end
